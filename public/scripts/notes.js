4c6dc568224f71b4f41c2af07ab6fe177e595cec
Ludovic Lafole <ludovic.lafole@gmail.com>
2015-10-11 14:57:47 +0200

    better visual on notes form

--- public/scripts/notes.js
+++ public/scripts/notes.js
@@ -1,42 +1,106 @@
 Parse.initialize("O8dECYavaIBZwi34qBZnIkvwAQEWVXjAiHwOGbKb", "2Vmb5IdO9TgrBA4FVh2eovsNmPy2JpqW72H8Rlzi");
 
+
 $(document).ready(function () {
     var Indicateur = Parse.Object.extend("Indicateur");
     var NoteSaisie = Parse.Object.extend("NoteSaisie");
 
+    var links = "<ul>";
+
+    for (var i = 1; i <= 6; i++) {
+        links += "<li class='badge text-right'>"+i+"</li>";
+    }
+    links += "</ul>";
+
+
     var $indicatorsForm = $('#indicators');
-    var $indicatorsContainer = $indicatorsForm.find('.indicators-container');
+    var $formContainer = $indicatorsForm.find('.indicators-container');
+    var footer = $('footer'),
+        previousButton = footer.find('#previous'),
+        nextButton = footer.find('#next'),
+        finishButton = footer.find("#finish"),
+        width = $indicatorsForm.width();
+    footer.find('button').hide();
+
+
+    var currentPage = 0;
+    function actualizeButtons() {
+        if (currentPage <= 0) {
+            previousButton.hide();
+        } else {
+            previousButton.show();
+        }
+        if(currentPage < ($formContainer.find('section').length - 1)) {
+            nextButton.show();
+        } else {
+            nextButton.hide();
+            finishButton.show();
+        }
+    }
+    function next() {
+        console.log(currentPage +1, $formContainer.find('section').length);
+        if(currentPage + 1 >= $formContainer.find('section').length) return;
+        currentPage++;
+        actualizeButtons();
+        console.log(currentPage +1, $formContainer.find('section').length);
+        $formContainer.animate({
+            "left": "-=" +width+"px"
+        });
+    }
+    function previous() {
+        if(currentPage <= 0) return;
+        currentPage--;
+        actualizeButtons();
+        console.log(currentPage +1, $formContainer.find('section').length);
+        $formContainer.animate({
+            "left": "+=" +width+"px"
+        });
+    }
+
+    nextButton.click(next);
+    previousButton.click(previous);
+
+    var notes = {};
+    function noteSelect(id) {
+        return function() {
+            var element = $(this);
+            console.log("toto");
+            notes[id].note = element.text() | 0;
+            element.parent().find('.badge').removeClass('selected');
+            element.addClass('selected');
+            next();
+        }
+    }
 
     var allIndicators = new Parse.Query(Indicateur);
     allIndicators.include("objectifPrincipal");
     allIndicators.include("objectifPrincipal.objectifGeneral");
     allIndicators.find({
         success: function (results) {
+            $formContainer.width(width * (results.length+1));
             for (var i = 0; i < results.length; i++) {
                 var indicateur = results[i];
                 var objectifPrincipal = indicateur.get('objectifPrincipal');
                 var objectifGeneral = objectifPrincipal.get('objectifGeneral');
-                var idObjP = "objP-" + objectifPrincipal.id;
-                var idObjG = "objG-" + objectifGeneral.id;
-                var idInd = "ind-" + indicateur.id;
-                if ($('#' + idObjG).length == 0) {
-                    $indicatorsContainer.append("<div id='" + idObjG + "'><h1>" + objectifGeneral.get('label') + "</h1></div>");
-                }
-                if ($('#' + idObjP).length == 0) {
-                    $indicatorsContainer.find('#' + idObjG).append("<div id='" + idObjP + "'><h2>" + objectifPrincipal.get('label') + "</h2></div>");
-                }
-                $indicatorsContainer.find('#' + idObjP).append(
-                    "<p id='" + idInd + "'>" + indicateur.get('label') + "</p>" +
-                    "<select class='form-control' name='" + idInd + "'>" +
-                    "<option>1</option>" +
-                    "<option>2</option>" +
-                    "<option>3</option>" +
-                    "<option>4</option>" +
-                    "<option>5</option>" +
-                    "<option>6</option>" +
-                    "</select>"
-                );
+                var html = "<section>"+
+                    "<header>"+objectifGeneral.get('label')+"</header>"+
+                    "<div class='content'>"+
+                    "<h2>"+objectifPrincipal.get('label')+"</h2>"+
+                    "<p>"+indicateur.get('label')+"</p>"+
+                    links+
+                    "</div>"+
+                    "</section>";
+                var domElement = $(html);
+                notes[indicateur.id] = {
+                    indicateur: new Indicateur({id: indicateur.id}),
+                    note: 1
+                };
+                domElement.find('.badge').click(noteSelect(indicateur.id));
+                $formContainer.append(domElement);
             }
+            $formContainer.find('section').width(width);
+            footer.show();
+            actualizeButtons();
         },
         error: function (error) {
             console.log(error);
@@ -45,17 +109,11 @@ $(document).ready(function () {
 
     $indicatorsForm.submit(function (e) {
         e.preventDefault();
-
-        var notes = $.map($("select"), function (select) {
-            var id = select.name.substr(4);
-            var note = select.value;
-            return {
-                indicateur: new Indicateur({id: id}),
-                note: note
-            };
+        var notesList = Object.keys(notes).map(function (id) {
+            return notes[id];
         });
         var noteSaisies = new NoteSaisie();
-        noteSaisies.set('notes', notes);
+        noteSaisies.set('notes', notesList);
         noteSaisies.save(null, {
             success: function () {
                 console.log("C'est sauvegardé avec succès !");
