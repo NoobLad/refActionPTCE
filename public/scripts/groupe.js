800c07b2fd5c4a4cc87f9eee4513d733b744bd08
Rodrigo <reyes.rr@gmail.com>
2015-10-11 16:13:36 +0200

    Gestions des cas de succes/erreur sur le formulaire de creation de groupe

--- public/scripts/groupe.js
+++ public/scripts/groupe.js
@@ -5,16 +5,38 @@ $(document).ready(function () {
     var $groupesForm = $('#groupes');
     var Group = Parse.Object.extend("Group");
 
+    var emailMatcher = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
     $groupesForm.submit(function (e) {
         e.preventDefault();
+        if (!$('#nomGroupe').val()) {
+            $("#form-issue")
+                .html("Vous devez donner un nom à votre groupe.")
+                .show();
+            return false;
+        }
 
+        if (!emailMatcher.test($('#mail').val())) {
+            $("#form-issue")
+                .html("Vous devez donner un email valide.")
+                .show();
+            return false;
+        }
+
+        $("#groupes").hide();
+        $("#wait").show();
         var groupeSaisies = new Group();
         groupeSaisies.set('nomGroupe', $('#nomGroupe').val());
         groupeSaisies.set('description', $('#description').val());
         groupeSaisies.set('mail', $('#mail').val());
         groupeSaisies.save(null, {
             success: function () {
-                console.log("C'est sauvegardé avec succès !");
+                $("#wait").hide();
+                $(".alert-success").show();
+            },
+            error: function() {
+                $("#wait").hide();
+                $(".alert-warning").show();
             }
         });
     });    
