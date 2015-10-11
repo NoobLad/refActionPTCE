051336c7f2293f671e770d05456171a607028c05
Rodrigo <reyes.rr@gmail.com>
2015-10-11 16:22:00 +0200

    La creation d'un groupe affiche un bouton vers gestion.html

--- public/scripts/groupe.js
+++ public/scripts/groupe.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
     var Group = Parse.Object.extend("Group");
 
     var emailMatcher = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    var groupId = undefined;
 
     $groupesForm.submit(function (e) {
         e.preventDefault();
@@ -30,18 +31,22 @@ $(document).ready(function () {
         groupeSaisies.set('description', $('#description').val());
         groupeSaisies.set('mail', $('#mail').val());
         groupeSaisies.save(null, {
-            success: function () {
+            success: function (data) {
                 $("#wait").hide();
                 $(".alert-success").show();
+                console.log("data", arguments);
+                groupId = data.id;
             },
             error: function() {
                 $("#wait").hide();
                 $(".alert-warning").show();
             }
         });
-    });    
+    });
 
-
+    $("#goToGestion").click(function() {
+        document.location.href = "./gestion.html?groupId=" + groupId;
+    });
 
 });
 
